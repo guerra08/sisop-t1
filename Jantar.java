9329f26283b7e52fa2200162d6f8b69a8ede59f9
Eduardo Nohr Lessa <eduardo.lessa@acad.pucrs.br>
2019-09-15 18:19:58 -0300

    Resolucao da questao 1. Refatorar o codigo depois

--- Jantar.java
+++ Jantar.java
@@ -1,79 +1,85 @@
 import java.util.concurrent.*;
 
 public class Jantar {
-    static int num_threads = 50;
-    static volatile int porcoes = 0;
+    static int num_canibais = 20;
+    static int num_porcoes_por_travessa = 5;
+    static volatile int travessa = 5;
 
-    static class Canibal implements Runnable {
+    static class cozinheiro implements Runnable {
         Semaphore mutex;
-        Semaphore vazia;
-        Semaphore cozinhando;
-        private int tid;
+        Semaphore comida;
+        Semaphore cozinha;
+        Semaphore enchendo;
 
-        public Canibal(int id, Semaphore mtx, Semaphore vz, Semaphore cz){
-            this.tid = id;
-            mutex = mtx;
-            vazia = vz;
-            cozinhando = cz;
+        public cozinheiro(Semaphore mutex, Semaphore comida, Semaphore enchendo, Semaphore cozinha) {
+            this.mutex = mutex;
+            this.comida = comida;
+            this.cozinha = cozinha;
+            this.enchendo = enchendo;
         }
 
         public void run(){
-            while (true) {
-                try {
-                    cozinhando.acquire();
-                    mutex.acquire();
-                    System.out.println("Canibal " + tid + " está comendo ");
-                    porcoes++;
-                    if (porcoes == num_threads)
-                        vazia.release();
-                    mutex.release();
-                }catch (InterruptedException e) {
-                }
+            while(true){
+                try{
+                    System.out.println("Cozinheiro acordado!! Enchendo travessa");
+                    cozinha.acquire();
+                    travessa = num_porcoes_por_travessa;
+                    for (int i = 0; i < num_porcoes_por_travessa; i++){
+                        comida.release();
+                    }
+                    enchendo.release();
+                }catch(InterruptedException e){}
             }
         }
     }
 
-    static class Cozinheiro implements Runnable {
-        Semaphore mutex;
-        Semaphore vazia;
-        Semaphore cozinhando;
-        private int tid;
-        private int i;
 
-        public Cozinheiro(int id, Semaphore mtx, Semaphore vz, Semaphore cz){
+    static class canibal implements Runnable {
+        Semaphore mutex;
+        Semaphore comida;
+        Semaphore cozinha;
+        Semaphore enchendo;
+        private int tid;
+
+        public canibal(int id, Semaphore mutex, Semaphore comida, Semaphore enchendo, Semaphore cozinha) {
+            this.mutex = mutex;
+            this.comida = comida;
+            this.cozinha = cozinha;
+            this.enchendo = enchendo;
             this.tid = id;
-            mutex = mtx;
-            vazia = vz;
-            cozinhando = cz;
         }
 
-        public void run(){
+        public void run() {
             while (true) {
-                try {
-                    vazia.acquire();
+                try{
+                    comida.acquire();
                     mutex.acquire();
-                    System.out.println("Cozinheiro está cozinhando");
-                    porcoes = 0;
-                    for (i = 0; i < num_threads; i++)
-                        cozinhando.release();
+                    if (travessa == 0) {
+                            System.out.println("Canibal " + tid + " acordando o cozinheiro.");
+                            cozinha.release();
+                            enchendo.acquire();
+                            travessa = num_porcoes_por_travessa;
+                    }
+                    travessa--;
+                    System.out.println("Canibal " + tid + " comendo a porcao. Restam " + travessa + " na travessa.");
                     mutex.release();
-                }catch (InterruptedException e) {
-                }
+                }catch(InterruptedException e){}
             }
         }
     }
 
     public static void main(String[] args) {
         Semaphore mutex = new Semaphore(1);
-        Semaphore vazia = new Semaphore(0);
-        Semaphore cozinhando = new Semaphore(num_threads);
+        Semaphore comida = new Semaphore(num_canibais);
+        Semaphore cozinha = new Semaphore(0);
+        Semaphore enchendo = new Semaphore(0);
         int i;
 
-        Thread p = new Thread(new Cozinheiro(0, mutex, vazia, cozinhando));
+        Thread p = new Thread(new cozinheiro(mutex, comida, enchendo, cozinha));
         p.start();
-        for (i = 1; i <= num_threads; i++){
-            Thread t = new Thread(new Canibal(i, mutex, vazia, cozinhando));
-            t.start();
+        for (i = 1; i <= num_canibais; i++) {
+            Thread c = new Thread(new canibal(i, mutex, comida, enchendo, cozinha));
+            c.start();
         }
     }
 }
